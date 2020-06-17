f1d47723263d591b7f4f5a8742797f37d3754190
BG00924 <BG00924@users.noreply.github.com>
2020-06-17 16:30:20 -0500

    shop function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -103,6 +103,11 @@ var startGame = function() {
         var pickedEnemyName = enemyNames[i];
         enemyHealth=50;
         fight(pickedEnemyName);
+
+        // if we're not at the last enemy in the array execute shop()
+        if (playerHealth > 0 && i < enemyNames.length - 1) {
+            shop();
+        }
     }    
     // causes it to play again; removed so endGame can function, function relocated inside endGame
     // startGame ();
@@ -135,9 +140,51 @@ var endGame = function () {
 
 };
 
+// shop function expresion
+var shop = function() {
+    // ask the player what they'd like to do
+    var shopOptionPrompt = window.prompt ("Would you like to REFILL your health, Upgrade your attack, or Leave the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    // use switch to carry out action
+    switch (shopOptionPrompt) {
+        case "REFILL": //new case
+        case "refill":
+            if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7) {
+                window.alert("Upgrade player's attack by 6 for 7 dollars.");
+                //increase Attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!")
+            }
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("leaving the store.");
+            // do nothing, so the function will end
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+            //call shop() again to force player to pick a valid option
+            shop();
+            break;
+    }
+};
 
 
-// start the game when the page loads
+// start the game when the page loads; this is a function call
 startGame();
 
 // Game States
